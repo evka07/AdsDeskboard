a5ac037b5e3eaa940aa40f265187e833d5d87cfa
evka07 <138365894+evka07@users.noreply.github.com>
2024-03-13 12:39:14 +0100

    env_secret added

--- db.js
+++ db.js
@@ -1,9 +1,10 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 
+
 const connectToDB = () => {
   mongoose.connect(
-    `mongodb+srv://threeways:${process.env.SECRET_DB}@cluster1.fzf2cia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`,
+    `mongodb+srv://threeways007:${process.env.DB_PASS}@cluster1.fzf2cia.mongodb.net/?retryWrites=true&w=majority&appName=Cluster1`,
     {
       useNewUrlParser: true,
       useUnifiedTopology: true,
